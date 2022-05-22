397103a3aca92f3b283d0a5d25404eb3fa6e96b7
igorpapsot <igorpapsot@gmail.com>
2022-05-22 19:11:05 +0200

    Dodato prikazivanje komponenti na klik

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private router: Router){
+
+  }
+
+  public showLogin = false;
+
+  public showRegister = false;
+
+  homeClick(){
+    this.showLogin = false;
+    this.showRegister = false;
+  }
+
+  signUpClick(){
+    this.showRegister = true;
+    this.showLogin = false;
+  }
+
+  signInClick(){
+    this.showLogin = true;
+    this.showRegister = false;
+  }
+
 }
